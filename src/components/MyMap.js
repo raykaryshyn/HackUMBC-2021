894cc44820900b4acf6c772357c0c0a56053b508
Ray Karyshyn <raykaryshyn@gmail.com>
2021-10-30 07:36:42 -0400

    add mui

---
+++ src/components/MyMap.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
+
+function Map() {
+    const [data, setData] = useState([]);
+    const getData = () => {
+        fetch('data.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        )
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson)
+            });
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    return (
+        <GoogleMap defaultZoom={10} defaultCenter={{ lat: 39.254749, lng: -76.710838 }}>
+            {data.map((point, i) => (
+                <Marker key={i} position={{
+                    lat: point.Latitude,
+                    lng: point.Longitude
+                }} />
+            ))}
+        </GoogleMap>
+    );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+function MyMap() {
+    return (
+        <div className="App" style={{ width: '100vw', height: '100vh' }}>
+            {
+                /*data && data.length > 0 && data.map((item) => <p>item</p>)*/
+            }
+            <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyC3RDeRum5X-MLlcKX8_Z5c2RWtPNdxUY4`}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100%` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            >
+            </WrappedMap>
+        </div>
+    );
+}
+
+export default MyMap;
